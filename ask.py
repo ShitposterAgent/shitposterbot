e6418054f7753e72c1b6772f91d867eb10ff98cc
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-05-05 21:51:26 +0100

    🧩 add initial implementation of ask.py with browser extension queries

---
+++ ask.py
@@ -0,0 +1,5 @@
+# can I install an extension in a browser (e.g chromium) programmatically?
+# I asked a queestion. please respond below
+
+# can I control a browser, maybe chromium progrgammatically?
+# I asked a queestion. please respond below
